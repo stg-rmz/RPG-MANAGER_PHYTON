0ae1cdf0eb3faf3d4290b032f38804cca3a81e8d
stg-rmz <jameson2410lolmail.com>
2026-09-02 21:35:24 -0500

    [vista] agrega formulario de creacion

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template, request, redirect
+from models.personaje import Personaje
 from models.repositorio import lista_personajes
 
 app = Flask(__name__)
+repo = lista_personajes
 
 @app.route('/personajes', methods=['GET'])
-def obtener_personajes():
-    
-    datos = [
-        {
-            "nombre": p.nombre,
-            "clase": p.clase_personaje,
-            "nivel": p.nivel,
-            "vida": p.vida
-        } 
-        for p in lista_personajes
-    ]
-    return jsonify(datos)
+def listar_personajes():
+    return render_template('personajes.html', personajes=lista_personajes)
+
+@app.route('/personajes', methods=['POST'])
+def crear_personaje():
+    nombre = request.form['nombre']
+    clase = request.form['clase_personaje']
+    nivel = int(request.form['nivel'])
+    vida = int(request.form['vida'])
+
+    nuevo_p = Personaje(nombre, clase, nivel, vida)
+    lista_personajes.append(nuevo_p)
+
+    return redirect('/personajes')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True)
